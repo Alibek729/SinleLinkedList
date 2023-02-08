b3ba043c42af2e6d69d5798414d062afb6c60a39
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-02-08 22:33:30 +0600

    Single Linked List push, append, insert, isEmpty, count functions completed

---
+++ Contents.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+class Node<T> {
+    var value: T
+    var next: Node<T>?
+    
+    init(value: T, next: Node<T>? = nil){
+        self.value = value
+        self.next = next
+    }
+}
+
+struct SinglyLinkedList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    
+    public var count: Int {
+        var count = 0
+        var current = head
+        while current != nil {
+            count += 1
+            current = current?.next
+        }
+        return count
+    }
+    
+    public var isEmpty: Bool {
+        head == nil
+    }
+    
+    public init(first: Node<T>? = nil) {
+        self.head = first
+    }
+    
+    mutating func push(_ value: T) {
+        head = Node(value: value, next: head)
+        
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    mutating func append(_ value: T) {
+        let node = Node(value: value)
+        tail?.next = node
+        tail = node
+    }
+    
+    func insert(_ value: T, after index: Int) {
+        guard let node = node(at: index) else { return }
+        node.next = Node(value: value, next: node.next)
+    }
+    
+    func node(at index: Int) -> Node<T>? {
+        var currentIndex = 0
+        var currentNode = head
+        
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode?.next
+            currentIndex += 1
+        }
+        
+        return currentNode
+    }
+}
+
